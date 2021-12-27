ed6f0eabfcdae98024f713629e3e3df4707dba95
pawelpel <12129118+pawelpel@users.noreply.github.com>
2021-12-27 15:04:01 +0100

    Dictionaries and Hashmaps - Sherlock And Anagrams

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/sherlock_and_anagrams.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/sherlock-and-anagrams/
+import collections
+
+
+def sherlockAndAnagrams(s):
+    from collections import Counter
+    counter = 0
+    window_size = 1
+    while window_size < len(s):
+        pairs = []
+        for i in range(len(s)-window_size+1):
+            x = ''.join(sorted(s[i:window_size+i]))
+            pairs.append(x)
+        for v in Counter(pairs).values():
+            if v > 1:
+                counter += v*(v-1)//2
+        window_size += 1
+    return counter
+
+
+assert sherlockAndAnagrams('ifailuhkqq') == 3
+assert sherlockAndAnagrams('kkkk') == 10
